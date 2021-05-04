55ae4a9b82ebd865e20520468a445547ce87549a
Kai Liu <kai.liu@suse.com>
2021-05-04 23:03:29 +0800

    PREEMPT_RT: set variant to -rt.

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,7 +1,7 @@
 # The version of the main tarball to use
 SRCVERSION=5.12
 # variant of the kernel-source package, either empty or "-rt"
-VARIANT=
+VARIANT="-rt"
 # enable kernel module compression
 COMPRESS_MODULES="xz"
 COMPRESS_VMLINUX="xz"
